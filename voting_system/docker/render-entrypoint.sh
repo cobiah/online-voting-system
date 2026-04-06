fa6c708f5e1cfeb30e98f63d8544161f8799ef19
cobiah <kobiahclaris@gmail.com>
2026-04-06 10:05:25 +0300

    Harden Render Apache port binding in entrypoint

--- voting_system/docker/render-entrypoint.sh
+++ voting_system/docker/render-entrypoint.sh
@@ -3,7 +3,10 @@ set -eu
 
 PORT_TO_USE="${PORT:-10000}"
 
-sed -i "s/Listen 80/Listen ${PORT_TO_USE}/" /etc/apache2/ports.conf
-sed -i "s/<VirtualHost \\*:80>/<VirtualHost *:${PORT_TO_USE}>/" /etc/apache2/sites-available/000-default.conf
+# Always rewrite Apache port settings, regardless of their previous value.
+sed -E -i "s/^Listen [0-9]+$/Listen ${PORT_TO_USE}/" /etc/apache2/ports.conf
+sed -E -i "s#<VirtualHost \\*:[0-9]+>#<VirtualHost *:${PORT_TO_USE}>#" /etc/apache2/sites-available/000-default.conf
+
+apache2ctl -t
 
 exec "$@"
